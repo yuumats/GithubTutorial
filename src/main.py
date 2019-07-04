910f592f3980eae02854c15f5609418aee85989b
tomori <nt24101310@gmail.com>
2019-07-04 18:55:51 +0900

    add sample program

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# サンプルコード
+
+while True:
+    print("数値を入力してください")
+    input_num = int(input())
+
+    if input_num == 0:
+        print('プログラムを終了します')
+        break
+    # -- ここに処理を追加してみよう --
+    else:
+        print('該当するコマンドがありません')
