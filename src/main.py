6e38ceb099f4426294aba72731aa69763227596b
tomori <nt24101310@gmail.com>
2019-07-12 10:51:09 +0900

    make case input_num = 1 to print Hello World
    1が入力された時、HelloWorldと出力する条件分岐を作成

--- src/main.py
+++ src/main.py
@@ -8,5 +8,7 @@ while True:
         print('プログラムを終了します')
         break
     # -- ここに処理を追加してみよう --
+    elif input_num == 1:
+        print('Hello World')
     else:
         print('該当するコマンドがありません')
